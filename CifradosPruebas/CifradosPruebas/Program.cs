9aa890145124a42fc3151f288979112b619cb7f0
GuillermoCT <squall_600@hotmail.com>
2021-10-27 12:46:42 +0200

    Creación del proyecto

---
+++ CifradosPruebas/CifradosPruebas/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CifradosPruebas
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
