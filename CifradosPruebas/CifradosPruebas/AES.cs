bb5b9c73357c6c65816b4763c0531eeabaa48f64
GuillermoCT <squall_600@hotmail.com>
2021-11-03 11:17:39 +0100

    Pruebas cifrado
    Pruebas

---
+++ CifradosPruebas/CifradosPruebas/AES.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+using System.Text;
+
+
+namespace CifradosPruebas
+{
+    public class AES
+    {
+        /// <summary>
+        /// aes-256-cbc 解码
+        /// </summary>
+        /// <param name="cipherData"></param>
+        /// <param name="keyString"></param>
+        /// <param name="ivString"></param>
+        /// <returns></returns>
+        public static string Decrypt(string cipherData, string keyString, string ivString)
+        {
+            byte[] key = Encoding.UTF8.GetBytes(keyString);
+            byte[] iv = Encoding.UTF8.GetBytes(ivString);
+
+            try
+            {
+                using (var rijndaelManaged =
+                       new RijndaelManaged { Key = key, IV = iv, Mode = CipherMode.CBC })
+                using (var memoryStream =
+                       new MemoryStream(Convert.FromBase64String(cipherData)))
+                using (var cryptoStream =
+                       new CryptoStream(memoryStream,
+                           rijndaelManaged.CreateDecryptor(key, iv),
+                           CryptoStreamMode.Read))
+                {
+                    return new StreamReader(cryptoStream).ReadToEnd();
+                }
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                return null;
+            }
+            // You may want to catch more exceptions here...
+        }
+
+        /// <summary>
+        /// aes-256-cbc 加密
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="KeyString"></param>
+        /// <param name="IVString"></param>
+        /// <returns></returns>
+        public static string EncryptString(string message, string KeyString, string IVString)
+        {
+            byte[] Key = ASCIIEncoding.UTF8.GetBytes(KeyString);
+            byte[] IV = ASCIIEncoding.UTF8.GetBytes(IVString);
+
+            string encrypted = null;
+            RijndaelManaged rj = new RijndaelManaged();
+            rj.Key = Key;
+            rj.IV = IV;
+            rj.Mode = CipherMode.CBC;
+
+            try
+            {
+                MemoryStream ms = new MemoryStream();
+
+                using (CryptoStream cs = new CryptoStream(ms, rj.CreateEncryptor(Key, IV), CryptoStreamMode.Write))
+                {
+                    using (StreamWriter sw = new StreamWriter(cs))
+                    {
+                        sw.Write(message);
+                        sw.Close();
+                    }
+                    cs.Close();
+                }
+                byte[] encoded = ms.ToArray();
+                encrypted = Convert.ToBase64String(encoded);
+
+                ms.Close();
+            }
+            catch (CryptographicException e)
+            {
+                Console.WriteLine("A Cryptographic error occurred: {0}", e.Message);
+                return null;
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine("A file error occurred: {0}", e.Message);
+                return null;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("An error occurred: {0}", e.Message);
+            }
+            finally
+            {
+                rj.Clear();
+            }
+            return encrypted;
+        }
+    }
+}
--- CifradosPruebas/CifradosPruebas/Program.cs
+++ CifradosPruebas/CifradosPruebas/Program.cs
@@ -9,58 +9,111 @@ namespace CifradosPruebas
     {
         static void Main(string[] args)
         {
-            string cadena = "3e6f15a4fc73aaa7a95212bd910df36676f3814894128b23f71b26ee9a364b01-6";
-            string pass= "KujIDA6573#muxiG20211027";
-            //string intIV = "'AES-256-CBC'";
+            //string cadena = "3e6f15a4fc73aaa7a95212bd910df36676f3814894128b23f71b26ee9a364b01";
+            string cadena = "6-6";
+            string pass = "KujIDA6573#muxiG20211102";
+            String valorSalida = "vrOQ1V4YQ6X1ZmBFCOIyfw==";
+            //string pass2 = Convert.ToBase64String(Encoding.UTF8.GetBytes(pass));
 
-            string salida = encryptAES256CBC(cadena, pass);
-            Console.WriteLine("Token: " + salida);
+            //string recta = "vrOQ1V4YQ6X1ZmBFCOIyfw==";
+            //string recta = "zLC6y4tL+WxKgC9hp48smg==";
+
+            //string recta = Aes256Encrypt(cadena, pass);
+            //string retb = Aes256Decrypt(recta, pass);
+
+            string recta = Encrypt(cadena);
+            //string recta2 = "vrOQ1V4YQ6X1ZmBFCOIyfw==";
+
+            //AES.EncryptString(cadena, pass, "00000000000000000000000000000000");
+            //string retb2 = AES.Decrypt(recta2, pass, "0000000000000000");
+
         }
 
-
-        public static string encryptAES256CBC(string Cadena, string pass)
+        public static string Encrypt(string clearText)
         {
-
-            // Recrear
-            string token_encode;
-            var key = new byte[32];
-
-            key = Encoding.UTF8.GetBytes(pass);
-
-            byte[] iv = new byte[16] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
-            Aes aesAlg = Aes.Create();
-
-
-            using ((aesAlg))
+            string EncryptionKey = "KujIDA6573#muxiG20211102";
+            byte[] clearBytes = ASCIIEncoding.UTF8.GetBytes(clearText);
+            using (Aes encryptor = Aes.Create())
             {
-                aesAlg.Key = key;
-                aesAlg.IV = iv;
-                aesAlg.Mode = CipherMode.CBC;
-
-                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
-                MemoryStream msEncrypt = new MemoryStream();
-
-                using ((msEncrypt))
+                Rfc2898DeriveBytes pdb = new
+                    Rfc2898DeriveBytes(EncryptionKey, new byte[]
+                    { 0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76 });
+                encryptor.Key = pdb.GetBytes(32);
+                encryptor.IV = pdb.GetBytes(16);
+                using (MemoryStream ms = new MemoryStream())
                 {
-                    CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write);
-                    using ((csEncrypt))
+                    using (CryptoStream cs = new CryptoStream(ms, encryptor.CreateEncryptor(), CryptoStreamMode.Write))
                     {
-                        StreamWriter swEncrypt = new StreamWriter(csEncrypt);
-                        using ((swEncrypt))
-                            swEncrypt.Write(Cadena);
-                        byte[] encrypted;
-                        encrypted = msEncrypt.ToArray();
-
-                        string Encrypt = Convert.ToBase64String(encrypted);
-                        token_encode = Encrypt;//Uri.EscapeDataString(Encrypt);
+                        cs.Write(clearBytes, 0, clearBytes.Length);
+                        cs.Close();
                     }
+                    clearText = Convert.ToBase64String(ms.ToArray());
                 }
             }
-
-            return token_encode;
+            return clearText;
         }
 
+        public static string Aes256Encrypt(string input, string password)
+        {
+            using (var aes = new RijndaelManaged())
+            {
+               
+                aes.Mode = CipherMode.CBC;
+                aes.BlockSize = 128;
+                aes.KeySize = 256;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.IV = new byte[16] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
 
+                if (password.Length > 16)
+                    password = password.Substring(0, 16);
+
+                byte[] pwd = Encoding.UTF8.GetBytes(password);
+                aes.Key = pwd;
+
+                byte[] bytesToBeEncrypted = Encoding.UTF8.GetBytes(input);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, aes.CreateEncryptor(aes.Key, aes.IV), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeEncrypted, 0, bytesToBeEncrypted.Length);
+                    }
+                    var msArray = ms.ToArray();
+                    return Convert.ToBase64String(msArray);
+                }
+            }
+        }
+
+        public static string Aes256Decrypt(string input, string password)
+        {
+            using (var aes = new RijndaelManaged())
+            {
+                aes.GenerateIV();
+                aes.Mode = CipherMode.CBC;
+                aes.BlockSize = 128;
+                aes.KeySize = 256;
+                aes.Padding = PaddingMode.PKCS7;
+                aes.IV = new byte[16] { 0x00, 0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08, 0x09, 0x0A, 0x0B, 0x0C, 0x0D, 0x0E, 0x0F };
+
+                if (password.Length > 16)
+                    password = password.Substring(0, 16);
+
+                byte[] pwd = Encoding.UTF8.GetBytes(password);
+                aes.Key = pwd;
+
+                byte[] bytesToBeDecrypted = Convert.FromBase64String(input);
+
+                using (MemoryStream ms = new MemoryStream())
+                {
+                    using (var cs = new CryptoStream(ms, aes.CreateDecryptor(aes.Key, aes.IV), CryptoStreamMode.Write))
+                    {
+                        cs.Write(bytesToBeDecrypted, 0, bytesToBeDecrypted.Length);
+                    }
+                    var msArray = ms.ToArray();
+                    return Encoding.UTF8.GetString(msArray, 0, msArray.Length);
+                }
+            }
+        }
 
 
     }
